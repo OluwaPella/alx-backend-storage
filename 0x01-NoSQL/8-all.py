84946a7f4861a2279600e54ad62d7756cdef321f
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-16 03:00:26 +0100

    8-all.py

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Python function that lists all documents 
+"""
+def list_all(mongo_collection):
+    """function to find
+    """
+    return list(mongo_collection.find())
