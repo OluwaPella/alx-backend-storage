a4cc27cef9cff7ffe6758c29a261d2ee78114214
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-16 03:40:38 +0100

    10-update_school.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+ Python function that changes all topics of a school document
+"""
+def update_topics(mongo_collection, name, topics):
+        mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}}):wq
