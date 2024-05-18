147eadee95d03a8c36d66af3b08b99e9acd995b0
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-18 18:27:03 +0100

    add: cache code

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+doc 
+"""
+import uuid
+import redis
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+        
+def set(self, data: Union[str, bytes, int, float ]) -> str:
+    keyy = str(uuid.uuid4())
+    self._redis.set(keyy, data)
+    return keyy
+
