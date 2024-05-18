4d185cbaf09387546ab984227a51bd4c2d84b5d1
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-18 18:33:05 +0100

    add: cache code

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,14 +4,15 @@ doc
 """
 import uuid
 import redis
+from typing import Union
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
-        
-def set(self, data: Union[str, bytes, int, float ]) -> str:
-    keyy = str(uuid.uuid4())
-    self._redis.set(keyy, data)
-    return keyy
+    
+    def set(self, data: Union[str, bytes, int, float ]) -> str:
+        keyy = str(uuid.uuid4())
+        self._redis.set(keyy, data)
+        return keyy 
 
