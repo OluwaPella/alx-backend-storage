b34cf12b3dc64ed692a5d2ad58f6b3ff273662e3
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-20 23:05:28 +0100

    add: exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,8 +7,39 @@ import redis
 from typing import Union
 from functools  import wraps
 
+def count_calls(method):
+        """
+        Decorator function that counts the number of calls made to a method.
+        
+        Parameters:
+            method (function): The method to be decorated.
+
+        Returns:
+            function: The decorated method.
+
+        """
+        @wraps(method)
+        def wrapper(self, *args, **kwargs): 
+            self._redis.incr(method.__qualname__)
+            result = method(self, *args, **kwargs)
+            return result
+        return wrapper
+
+
 class Cache:
+
     def __init__(self):
+        """
+        Initializes a new instance of the Cache class.
+
+        This constructor creates a new instance of the Cache class and initializes the `_redis` attribute with a new instance of the `redis.Redis` class. It also flushes the entire Redis database by calling the `flushdb()` method on the `_redis` object.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
     @count_calls
@@ -17,8 +48,19 @@ class Cache:
         keyy = str(uuid.uuid4())
         self._redis.set(keyy, data)
         return keyy
+    
 
     def get(self, key, fn=None):
+        """
+        Retrieves the value associated with the given key from the cache.
+
+        Parameters:
+            key (str): The key to retrieve the value for.
+            fn (callable, optional): A function to apply to the retrieved value. Defaults to None.
+
+        Returns:
+            The value associated with the key, or the result of applying the function `fn` to the value if `fn` is provided. Returns None if the key does not exist in the cache.
+        """
         data = self._redis.get(key)
         if data is None:
             return None
@@ -28,15 +70,25 @@ class Cache:
             return fn(data) 
     
     def get_str(self, key):
+        """
+        Retrieves the value associated with the given key from the cache and decodes it as a UTF-8 string.
+
+        Parameters:
+            key (str): The key to retrieve the value for.
+
+        Returns:
+            str: The decoded value associated with the key, or None if the key does not exist in the cache.
+        """
         return self.get(key, lambda x: x.decode('utf-8'))
     def get_int(self,key):
+        """
+        Retrieves the value associated with the given key from the cache and converts it to an integer.
+
+        Parameters:
+            key (str): The key to retrieve the value for.
+
+        Returns:
+            int: The integer value associated with the key, or None if the key does not exist in the cache.
+        """
         return self.get(key, lambda x: int(x))
-     
-    def count_calls(method):
-        @wraps(method)
-        def wrapper(self, *args, **kwargs): 
-            self._redis.incr(method.__qualname__)
-            result = method(self, *args, **kwargs)
-            return result
-        return wrapper
 
